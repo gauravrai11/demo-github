604af76c9a5ed1f82b5be6e636698a81607d7be8
gauravrai11 <gauravrai997@gmail.com>
2024-04-22 16:16:11 +0530

    Revert "added subtract function"
    This reverts commit e0af4ed0265274c3dc28de5d860de4654774be07.

--- Calc.java
+++ Calc.java
@@ -4,7 +4,6 @@ public class GFG {
         System.out.println("Hello World"); 
         GFG ob = new GFG();
         int res = ob.sum(28, 49);
-        int res = ob.subract(28, 49);
         System.out.println(res);
     } 
 }
@@ -13,12 +12,4 @@ class GFG {
     public static int sum(int num1, int num2)
     {
         return num1+num2;
-        
     }
-
-    public static int subtract(int num1, int num2)
-    {
-        return num1-num2;
-        
-    }
-}
