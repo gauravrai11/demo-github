e0af4ed0265274c3dc28de5d860de4654774be07
gauravrai11 <gauravrai997@gamil.com>
2024-04-22 15:45:31 +0530

    added subtract function

--- Calc.java
+++ Calc.java
@@ -4,6 +4,7 @@ public class GFG {
         System.out.println("Hello World"); 
         GFG ob = new GFG();
         int res = ob.sum(28, 49);
+        int res = ob.subract(28, 49);
         System.out.println(res);
     } 
 }
@@ -12,4 +13,12 @@ class GFG {
     public static int sum(int num1, int num2)
     {
         return num1+num2;
+        
     }
+
+    public static int subtract(int num1, int num2)
+    {
+        return num1-num2;
+        
+    }
+}
