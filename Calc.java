3f8516e1e2f6024bed1ec49d9d4bead98e968622
gauravrai11 <gauravrai997@gamil.com>
2024-04-22 15:35:23 +0530

    added add function

--- Calc.java
+++ Calc.java
@@ -2,5 +2,14 @@ public class GFG {
     public static void main(String args[]) 
     { 
         System.out.println("Hello World"); 
+        GFG ob = new GFG();
+        int res = ob.sum(28, 49);
+        System.out.println(res);
     } 
 }
+
+class GFG {
+    public static int sum(int num1, int num2)
+    {
+        return num1+num2;
+    }
